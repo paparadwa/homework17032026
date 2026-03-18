d0b675d997af3680d5ee0bc50e764cce5cc7e8ca
paparadwa <timoshkov_a@bk.ru>
2026-03-18 19:22:26 +0300

    updated 18.03

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,27 @@ public class Main {
         System.out.println("-----TASK 1-----");
         int[] inputArray1 = {10_000, 30_000, 15_000, 20_000, 25_000};
         float[] outputArray1 = new float[4];
-        int maxCash = -10000;
-        int minCash = 1_000_000_000;
+        int maxCash = inputArray1[0];
+        int minCash = inputArray1[0];
         int index1 = 0;
         for (int j : inputArray1) {
             outputArray1[0] += j;
             if (j > maxCash) {
                 maxCash = j;
-                outputArray1[1] = maxCash;
             }
             if (j < minCash) {
                 minCash = j;
-                outputArray1[2] = minCash;
             }
-            if (index1 == outputArray1.length - 1) {
-                outputArray1[index1] = outputArray1[0] / inputArray1.length;
-            }
-            index1++;
         }
+        outputArray1[1] = maxCash;
+        outputArray1[2] = minCash;
+        outputArray1[3] = outputArray1[0] / inputArray1.length;
         System.out.println(Arrays.toString(inputArray1));
         System.out.println(Arrays.toString(outputArray1));
         //task 2
         System.out.println("-----TASK 2-----");
         int[] inputArray2 = {10_000, 30_000, 15_000, 20_000, 25_000};
-        float[] outputArray2 = new float[5];
+        float[] outputArray2 = new float[inputArray2.length];
         int index2 = 0;
         for (int j : inputArray2) {
             outputArray2[index2] = j * 0.87f;
@@ -40,7 +37,7 @@ public class Main {
         //task 3
         System.out.println("-----TASK 3-----");
         int[] inputArray3 = {2000, 3000, 7000, 8000, 4000};
-        boolean[] outputArray3 = new boolean[5];
+        boolean[] outputArray3 = new boolean[inputArray3.length];
         int index3 = 0;
         int bonus = 5000;
         for (int j : inputArray3) {
@@ -52,28 +49,33 @@ public class Main {
         //task 4
         System.out.println("-----TASK 4-----");
         int[] inputArray4 = {1000, -1500, 2000, 3000, -7500};
-        boolean[] outputArray4 = new boolean[5];
-        int index4 = 0;
+        boolean[] outputArray4 = new boolean[1];
+        boolean positiveBalance = true;
         for (int j : inputArray4) {
             if (j > 0) {
-                outputArray4[index4] = true;
+                positiveBalance = true;
             } else {
-                outputArray4[index4] = false;
+                positiveBalance = false;
                 break;
             }
-            index4++;
         }
+        outputArray4[0] = positiveBalance;
         System.out.println(Arrays.toString(inputArray4));
         System.out.println(Arrays.toString(outputArray4));
         //task 5
         System.out.println("-----TASK 5-----");
         int[] inputArray5 = {5000, 0, -1500, 2000, 7000};
-        boolean[] outputArray5 = new boolean[5];
-        int index5 = 0;
+        boolean[] outputArray5 = new boolean[1];
+        boolean rent = true;
         for(int profit : inputArray5){
-            outputArray5[index5] = profit > 0;
-            index5++;
+            if (profit > 0) {
+                rent = true;
+            } else {
+                rent = false;
+                break;
+            }
         }
+        outputArray5[0] = rent;
         System.out.println(Arrays.toString(inputArray5));
         System.out.println(Arrays.toString(outputArray5));
     }
